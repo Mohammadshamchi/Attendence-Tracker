491d0195d03f878a333adbeec7da8f44bbae6651
Mohammadshamchi <shamchimohammad@gmail.com>
2024-09-24 21:24:11 +0330

    Only Class Detail Page information Changes

--- src/ClassDetail.tsx
+++ src/ClassDetail.tsx
@@ -36,20 +36,10 @@ function ClassDetail() {
                 </TableHeader>
                 <TableBody>
                     <TableRow>
-                        <TableCell className="font-medium">INV001</TableCell>
-                        <TableCell>Paid</TableCell>
+                        <TableCell className="font-medium">Student 1</TableCell>
+                        <TableCell>✅✅✅❌✅</TableCell>
                         <TableCell className="text-right">
-                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_0_2906)">
-                                    <path d="M10 13.3334V10M10 6.66669H10.0084M18.3334 10C18.3334 14.6024 14.6024 18.3334 10 18.3334C5.39765 18.3334 1.66669 14.6024 1.66669 10C1.66669 5.39765 5.39765 1.66669 10 1.66669C14.6024 1.66669 18.3334 5.39765 18.3334 10Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_0_2906">
-                                        <rect width="20" height="20" fill="white" />
-                                    </clipPath>
-                                </defs>
-                            </svg>
-
+                            <button className="btn btn-primary">View</button>
                         </TableCell>
                     </TableRow>
                 </TableBody>
