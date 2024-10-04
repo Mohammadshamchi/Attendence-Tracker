11888e0e6954a5a002ffd9fe880e6d649b1c6831
Mohammadshamchi <shamchimohammad@gmail.com>
2024-10-04 14:23:29 +0330

    Seprate Student Search Component+ Class Search Component

--- src/components/AddStudent.tsx
+++ src/components/AddStudent.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
+import ClassSearch from "./ClassSearch";
+import { ClassData } from "@/FakeData";
 
 export default function AddStudent() {
 
@@ -10,6 +12,7 @@ export default function AddStudent() {
     const [errors, setErrors] = useState<{ [key: string]: string }>({});
     const [isLoading, setIsLoading] = useState(false);
     const [submitError, setSubmitError] = useState("");
+    const [selectedClass, setSelectedClass] = useState<ClassData[]>([]);
 
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -21,6 +24,7 @@ export default function AddStudent() {
                     first_name: firstName.trim(),
                     last_name: lastName.trim(),
                     phone_number: phoneNumber.trim(),
+                    class_signed_up: selectedClass.map(c => c.name),
                     notes: notes.trim()
                 });
                 console.log("Student added successfully:", response.data);
@@ -55,6 +59,7 @@ export default function AddStudent() {
         if (!firstName.trim()) newErrors.firstName = "First name is required and cannot be just spaces";
         if (!lastName.trim()) newErrors.lastName = "Last name is required and cannot be just spaces";
         if (!phoneNumber.trim()) newErrors.phoneNumber = "Phone number is required";
+        if (selectedClass.length === 0) newErrors.selectedClass = "Please select at least one Class";
         else if (phoneNumber.trim().length < 10) newErrors.phoneNumber = "Phone number must be at least 10 characters long";
 
         setErrors(newErrors);
@@ -101,6 +106,12 @@ export default function AddStudent() {
                         required
                     />
                     {errors.phoneNumber && <p className="text-red-500 text-xs mt-1">{errors.phoneNumber}</p>}
+                </div>
+                <div>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        Class Search
+                    </label>
+                    <ClassSearch selectedClass={selectedClass} setSelectedClass={setSelectedClass} />
                 </div>
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">
